61f1c25d4fd85e20e1a3ca23023b14060d0c3941
Devin-Jackson314 <you@example.com>
2022-09-23 09:56:22 -0400

    generating necessarry components

--- Assignments/scoot-multipage-project/src/app/app.module.ts
+++ Assignments/scoot-multipage-project/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CareersComponent } from './careers/careers.component';
+import { LocationComponent } from './location/location.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    CareersComponent,
+    LocationComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,

16c5c1d2110922e472a8bcb0796783c1e572ffe3
Devin-Jackson314 <you@example.com>
2022-09-23 12:00:05 -0400

    setting up my routes

--- Assignments/scoot-multipage-project/src/app/app-routing.module.ts
+++ Assignments/scoot-multipage-project/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CareersComponent } from './careers/careers.component';
+import { HomeComponent } from './home/home.component';
+import { LocationComponent } from './location/location.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "careers", component: CareersComponent },
+  { path: "location", component: LocationComponent },
+  {path:"navigation", component: NavigationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
